246a2304c8ac2eff4c1fd61bed132fafe6cdc38d
Jun-sung <gg0939@gmail.com>
2026-03-02 15:01:43 +0900

    스토어 리스트 페이지 생성
    스토어 페이지 생성

---
+++ src/main/java/com/deushu/store/controller/StoreApiController.java
@@ -0,0 +1,8 @@
+package com.deushu.store.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/store/api")
+public class StoreApiController {
+
+}
--- src/main/java/com/deushu/store/controller/StoreController.java
+++ src/main/java/com/deushu/store/controller/StoreController.java
@@ -1,5 +1,17 @@
 package com.deushu.store.controller;
 
-public class StoreController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/map")
+public class StoreController {
+	
+	@GetMapping("/list")
+	public String storeList() {
+		
+		return "store/storeList";
+	}
+	
 }
