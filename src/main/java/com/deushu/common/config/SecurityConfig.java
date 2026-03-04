b0f929d270d82d3fac779c316148fc67277a45f7
eugene6oz <injae8817@gmail.com>
2026-03-04 12:44:28 +0900

    시큐리티 주석처리
    제목내

--- src/main/java/com/deushu/common/config/SecurityConfig.java
+++ src/main/java/com/deushu/common/config/SecurityConfig.java
@@ -1,34 +1,34 @@
-package com.deushu.common.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-   
-	@Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/css/**", "/js/**", "/images/**");
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/main", "/main/**", "/store/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/stores/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/reviews", "/api/reviews/**").permitAll()
-                .anyRequest().authenticated()
-            );
-
-        return http.build();
-    }
-}
+//package com.deushu.common.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//   
+//	@Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().requestMatchers("/css/**", "/js/**", "/images/**");
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//            .csrf(csrf -> csrf.disable())
+//            .authorizeHttpRequests(auth -> auth
+//                .requestMatchers("/main", "/main/**", "/store/**").permitAll()
+//                .requestMatchers(HttpMethod.GET, "/api/stores/**").permitAll()
+//                .requestMatchers(HttpMethod.GET, "/api/reviews", "/api/reviews/**").permitAll()
+//                .anyRequest().authenticated()
+//            );
+//
+//        return http.build();
+//    }
+//}
