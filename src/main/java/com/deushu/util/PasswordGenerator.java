bdb92c938fb86b3c76af81770fe9a4e65c1e6212
Jun-sung <gg0939@gmail.com>
2026-03-21 18:16:16 +0900

    header admin페이지 버튼 수정.
    admin 페이지 버튼 수정과 스키마에 mock_business_registry 가게 30개 목업데이터 추가했습니다.
    도심지역으로 都心 (千代田区・中央区・港区) 에 위치한 가상 가게들 입니다.

--- src/main/java/com/deushu/util/PasswordGenerator.java
+++ src/main/java/com/deushu/util/PasswordGenerator.java
@@ -3,10 +3,12 @@ package com.deushu.util;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordGenerator {
-    /*
+    
+	
+/*	// admin용 비밀번호 생성기
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "1234";
+        String rawPassword = "12341234";
         String encoded = encoder.encode(rawPassword);
         System.out.println(encoded);
     } */
